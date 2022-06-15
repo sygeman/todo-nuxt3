8e4c5710998b6f36ae3af00df876c037f756aee1
Sygeman <sygeman@gmail.com>
2022-06-15 10:46:40 +0200

    ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
   buildModules: ["@pinia/nuxt"],
   modules: ["@nuxtjs/tailwindcss"],
 });
